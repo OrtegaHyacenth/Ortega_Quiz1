d6391b9fdf9d0469fe3a50651a5be69a4df0e086
home <home@192.168.3.91>
2023-09-04 16:27:31 +0800

    Version1juj

--- src/ortega_quiz1/ORTEGA_Quiz1.java
+++ src/ortega_quiz1/ORTEGA_Quiz1.java
@@ -15,7 +15,7 @@ public class ORTEGA_Quiz1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // jhghgjhgjgj
     }
     
 }
